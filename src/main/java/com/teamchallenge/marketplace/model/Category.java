00eadef3ba72c7ddb6535e4c9d490483df7c33dd
Nazariy <nazar27062002@gmail.com>
2023-10-14 16:03:25 +0300

    add entities User Seller Role Products Category

---
+++ src/main/java/com/teamchallenge/marketplace/model/Category.java
@@ -0,0 +1,4 @@
+package com.teamchallenge.marketplace.model;
+
+public class Category {
+}
---
+++ src/main/java/com/teamchallenge/marketplace/model/User.java
@@ -0,0 +1,50 @@
+package com.teamchallenge.marketplace.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String email, phone, birthDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(String birthDate) {
+        this.birthDate = birthDate;
+    }
+}
