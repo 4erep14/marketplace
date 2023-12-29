106e84294af39657259acd94a4b270351752ed8d
Bohdan Bazyl <b.bazyl.r@gmail.com>
2023-12-29 14:56:55 +0200

    add validation rules to register dto

--- src/main/java/com/teamchallenge/marketplace/dto/auth/RegisterRequest.java
+++ src/main/java/com/teamchallenge/marketplace/dto/auth/RegisterRequest.java
@@ -1,7 +1,8 @@
 package com.teamchallenge.marketplace.dto.auth;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,13 +12,26 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class RegisterRequest {
 
+    @Size(min = 2, max = 50)
+    @Pattern(regexp = "[A-Z][a-z]+(-?[A-Z][a-z]+)?", message = "Name should starts with capital letter")
+    @JsonProperty("first_name")
     private String firstName;
+
+    @Size(min = 2, max = 50)
+    @Pattern(regexp = "[A-Z][a-z]+(-?[A-Z][a-z]+)?", message = "Last name should starts with capital letter")
+    @JsonProperty("last_name")
     private String lastName;
+
+
+    @Size(min = 3, max = 64)
+    @Pattern(regexp = "^[\\w!#$%&amp;'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&amp;'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", message = "invalid email address")
     private String email;
+
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",  message = "Password should contain at least one capital letter, one digit and one special symbol. Its length should be between 8 and 16")
     private String password;
+
     private String phone;
 
 }
