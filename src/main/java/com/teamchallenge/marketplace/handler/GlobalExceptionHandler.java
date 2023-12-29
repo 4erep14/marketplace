20c2e8a5d156f0efc24d711c046492314f276e23
Bohdan Bazyl <b.bazyl.r@gmail.com>
2023-12-29 14:56:25 +0200

    add logs to GlobalExceptionHandler and UserValidator

--- src/main/java/com/teamchallenge/marketplace/handler/GlobalExceptionHandler.java
+++ src/main/java/com/teamchallenge/marketplace/handler/GlobalExceptionHandler.java
@@ -3,23 +3,35 @@ package com.teamchallenge.marketplace.handler;
 import com.teamchallenge.marketplace.exception.NullEntityReferenceException;
 import com.teamchallenge.marketplace.exception.UserNotValidException;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.nio.file.AccessDeniedException;
+import java.util.Arrays;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<?> handleIllegalStateException(IllegalStateException exception){
+        logger.error(exception.getMessage());
+
         return ResponseEntity
                 .badRequest()
                 .body(exception.getMessage());
     }
     @ExceptionHandler(UserNotValidException.class)
     public ResponseEntity<?> handleUserNotValidException(UserNotValidException exception){
+        logger.error(exception.getErrorMessages().toString());
+
         return ResponseEntity
                 .badRequest()
                 .body(exception.getErrorMessages());
@@ -27,6 +39,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(NullEntityReferenceException.class)
     public ResponseEntity<?> handleNullEntityReferenceException(NullEntityReferenceException exception) {
+        logger.error(exception.getMessage());
+
         return ResponseEntity
                 .badRequest()
                 .body(exception.getMessage());
@@ -34,6 +48,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<?> handleNullEntityNotFoundException(EntityNotFoundException exception) {
+        logger.error(exception.getMessage());
+
         return ResponseEntity
                 .badRequest()
                 .body(exception.getMessage());
@@ -41,6 +57,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(AccessDeniedException.class)
     public ResponseEntity<?> handleAccessDeniesException(AccessDeniedException exception) {
+        logger.error(exception.getMessage());
+
         return ResponseEntity
                 .status(HttpStatus.UNAUTHORIZED)
                 .body(exception.getMessage());
@@ -48,6 +66,9 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleInternalServerError(Exception exception) {
+        logger.error("{}, {}", exception.getClass().getName(), exception.getMessage());
+
+        System.out.println(Arrays.toString(exception.getStackTrace()));
         return ResponseEntity
                 .internalServerError()
                 .body(exception.getMessage());
--- src/main/java/com/teamchallenge/marketplace/validation/UserValidator.java
+++ src/main/java/com/teamchallenge/marketplace/validation/UserValidator.java
@@ -2,23 +2,30 @@ package com.teamchallenge.marketplace.validation;
 
 import com.teamchallenge.marketplace.exception.UserNotValidException;
 import jakarta.validation.*;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
 @Component
+@Slf4j
 public class UserValidator<T> {
     private final ValidatorFactory factory= Validation.buildDefaultValidatorFactory();
     private final Validator validator = factory.getValidator();
+    private final Logger logger = LoggerFactory.getLogger(UserValidator.class);
+
     public void validate(T userValidate){
-       Set<ConstraintViolation<T>> violations= validator.validate(userValidate);
-       if (!violations.isEmpty()){
-           var errorMessage=violations
-                   .stream()
-                   .map(ConstraintViolation::getMessage)
-                   .collect(Collectors.toSet());
-           throw new UserNotValidException(errorMessage);
+        logger.info("validate {} object data", userValidate.getClass().getName());
+        Set<ConstraintViolation<T>> violations= validator.validate(userValidate);
+        if (!violations.isEmpty()){
+            var errorMessage=violations
+                    .stream()
+                    .map(ConstraintViolation::getMessage)
+                    .collect(Collectors.toSet());
+            throw new UserNotValidException(errorMessage);
        }
     }
 }
